67f122bd7cc62cb4e2bc0f496c070f8a5906643d
Ubuntu <ubuntu@ip-172-31-20-16.us-east-2.compute.internal>
2018-12-04 20:14:46 +0000

    get_all_scores.py update

--- get_all_scores.py
+++ get_all_scores.py
@@ -4,32 +4,34 @@ import glob
 import cv2
 import numpy as np
 
-test_img_path = 'RUNS/crackSeg_2018_12_03_23.50/test_images/'
+test_img_path = 'RUNS/crackSeg_2018_12_04_16.02/test_images/'
 #test_img_path = 'RUNS/crackSeg_2018_11_26_21.52/test_images/'
 gt_img_path = 'DATA/data_crack_correct/training/'
 
 #num = len(test_imgs_names)
-num = 68
-test_imgs_names = sorted(glob.glob(test_img_path + '*.png'))[-num:]
-gt_imgs_names = sorted(glob.glob(gt_img_path + '*_gt.png'))[-num:]
+num = 56
+test_imgs_names = sorted(glob.glob(test_img_path + '*.png'))[:num]
+gt_imgs_names = sorted(glob.glob(gt_img_path + '*_gt.png'))[:num]
 #print(test_imgs_names)
 #print(gt_imgs_names)
 avg = 0
 avg_pixel_diff_pct = 0
-thresh = 128
+thresh = 60
 
 binary_output_path = os.path.join(test_img_path, 'binary_output')
 if not os.path.exists(binary_output_path):
     os.makedirs(binary_output_path)
 
-for img_name, gt_name in zip(test_imgs_names, gt_imgs_names):
+for i, (img_name, gt_name) in enumerate(zip(test_imgs_names, gt_imgs_names)):
+    if i % 4 != 3:
+        continue
     img = cv2.imread(img_name, 0)
     img[img <= thresh] = 0
     img[img > thresh] = 1
     
     basename = os.path.basename(img_name)
     binary_img_path = os.path.join(binary_output_path, basename)
-    cv2.imwrite(binary_img_path, img)
+    cv2.imwrite(binary_img_path, img * 255)
     gt = cv2.imread(gt_name, 0)
     gt[gt > 1] = 1
 
@@ -47,5 +49,5 @@ for img_name, gt_name in zip(test_imgs_names, gt_imgs_names):
     avg += scores[0]
     print(os.path.basename(img_name), pixel_diff_pct, scores[:-1])
 
-print('avg score: {}'.format(avg / float(num)))
-print('avg pixel diff pct: {}'.format(avg_pixel_diff_pct / float(num)))
+print('avg score: {}'.format(avg / float(num / 4)))
+print('avg pixel diff pct: {}'.format(avg_pixel_diff_pct / float(num / 4)))
